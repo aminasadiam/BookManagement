05edd0b33a82ada3b4b60c4d09f6613ff2e51b1b
Amin <aminasadiam@outlook.com>
2024-01-29 21:15:17 +0330

    Add Context

--- DataLayer/Context/BookContext.cs
+++ DataLayer/Context/BookContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,11 @@ namespace DataLayer.Context
 {
     public class BookContext : DbContext
     {
-        
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
     }
 }
