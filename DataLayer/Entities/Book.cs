1c52e2df0f3651cce257ee01372fcc1a5503709a
Amin <aminasadiam@outlook.com>
2024-01-29 21:11:27 +0330

    Add Book Model

--- DataLayer/Entities/Book.cs
+++ DataLayer/Entities/Book.cs
@@ -8,6 +8,15 @@ namespace DataLayer.Entities
 {
     public class Book
     {
+        public uint Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public uint AuthorId { get; set; }
+        public DateTime ReleaseDate { get; set; }
 
+        #region Relations
+
+
+        #endregion
     }
 }
