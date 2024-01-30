8cd8d425fbfe89cb5237f7c8aef946370422c59d
mehdddddddi7@gmail.com <mehdddddddi7@gmail.com>
2024-01-30 21:33:32 +0330

    GetBooks

---
+++ BookManagement/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Context;
+using DataLayer.Entities;
+using DataLayer.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookRepository bookRepository;
+
+        public BookController(BookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var book = bookRepository.GetAll();
+            return Ok(book);
+        }
+
+    }
+}
---
+++ DataLayer/Services/BookRepository.cs
@@ -0,0 +1,54 @@
+﻿using DataLayer.Context;
+using DataLayer.Entities;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Services
+{
+    public class BookRepository
+    {
+        private readonly BookContext context;
+
+        public BookRepository(BookContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Book> GetAll()
+        {
+            return context.Books.ToList();
+        }
+
+        public Book GetBookById(uint id)
+        {
+            return context.Books.Find(id);
+        }
+
+        public Book InsertBook(Book book)
+        {
+            context.Books.Add(book);
+            context.SaveChanges();
+            return book;
+        }
+
+        public Book RemoveBook(uint id)
+        {
+            var book = context.Books.Find(id);  
+            context.Books.Remove(book);
+            context.SaveChanges();
+            return book;
+        }
+
+        public Book UpdateBook(Book book)
+        {
+            context.Books.Update(book);
+            context.SaveChanges();
+            return book;
+        }
+    }
+
+}
